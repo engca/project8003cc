25a318508d0ad7d672d38b5d69c34d6f71f5fb40
JY.LEE <moyazoa89@gamil.com>
2017-04-14 15:10:33 +0900

    log4j setting

---
+++ Quest/src/controller/LogIntercepter.java
@@ -0,0 +1,35 @@
+package controller;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LogIntercepter extends HandlerInterceptorAdapter{
+	//preHandle,postHandle에서 log4j를 이용해서
+	//요청에 대한 정보와 응답에 대한 정보를 로그로 기록하세요 
+	
+	protected Log log = LogFactory.getLog(LogIntercepter.class);
+	 
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+            log.info("======================================          START         ======================================");
+            log.info(" Request URI \t:  " + request.getRequestURI());
+          return super.preHandle(request, response, handler);
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+             log.info("======================================           END          ======================================\n");
+	}
+}
---
+++ Quest/src/controller/LoginCheckInterceptor.java
@@ -0,0 +1,38 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class LoginCheckInterceptor extends HandlerInterceptorAdapter{
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		//
+		super.postHandle(request, response, handler, modelAndView);
+	}
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+//		return super.preHandle(request, response, handler);
+		//false를 리턴하면 request를 중단하고 리턴하여 컨트롤러와 남은 인터셉터들은 실행되지 않는다.
+				
+		if(request.getSession().getAttribute("id")== null){
+			response.sendRedirect("main.do");
+			return false;
+		}
+		else
+			return true;
+			
+	}
+	
+	
+}
