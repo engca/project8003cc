5f18fb37a01e23cd87c3eafa12c285b4ca5c0429
JY.Lee <moyazoa89@naver.com>
2017-04-19 14:57:13 +0900

    경로수정

--- Quest/src/controller/MyPageController.java
+++ Quest/src/controller/MyPageController.java
@@ -2,6 +2,7 @@ package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import service.IQuestService;
 
@@ -9,4 +10,34 @@ import service.IQuestService;
 public class MyPageController {
 	@Autowired
 	IQuestService service;
+	
+	
+	@RequestMapping("applyPopup.do")
+	public String applyPopup(){
+		return "/bootstrapResources/mypage/applyPopup.jsp";
+	}	
+	
+	@RequestMapping("bookmark.do")
+	public String bookmark(){
+		return "/bootstrapResources/mypage/bookmark.jsp";
+	}
+	
+	@RequestMapping("bookmarkPopup.do")
+	public String bookmarkPopup(){
+		return "/bootstrapResources/mypage/bookmarkPopup.jsp";
+	}
+	
+	@RequestMapping("complete.do")
+	public String complete(){
+		return "/bootstrapResources/mypage/complete.jsp";
+	}	
+	@RequestMapping("mylist.do")
+	public String mylist(){
+		return "/bootstrapResources/mypage/mylist.jsp";
+	}
+	@RequestMapping("profile.do")
+	public String profile(){
+		return "/bootstrapResources/mypage/profile.jsp";
+	}
+	
 }
