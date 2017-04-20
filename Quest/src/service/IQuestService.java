6abafa55ffb00e42b1a74b036d56dccf5f9a6c00
student <student@M90112>
2017-04-20 14:47:17 +0900

    11

--- Quest/src/service/IQuestService.java
+++ Quest/src/service/IQuestService.java
@@ -46,7 +46,7 @@ public interface IQuestService {
 	public int bookmark(int BoardNo, int userIndex); 
 
 	public int police(int BoardNo, int userIndex); 
-	public HashMap<String, Object> selectpolice(int BoardNo, int userIndex);
+	public HashMap<String, Object> selectpolice(int BoardNo, int userIndex); 
 	
 	public List<HashMap<String, Object>> selectAll();
 	
