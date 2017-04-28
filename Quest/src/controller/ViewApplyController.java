ae88190318efb2502680fc7ffbaaf8091b4d8bcd
JY.Lee <moyazoa89@naver.com>
2017-04-28 14:32:35 +0900

    신청자 보기 서블릿

---
+++ Quest/src/controller/ViewApplyController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import service.IQuestService;
+
+@Controller
+public class ViewApplyController {
+	@Autowired
+	IQuestService service;
+	
+	@RequestMapping("viewApplyPopup.do")
+	public String viewApplyPopup(@RequestParam HashMap<String, Object>params){
+		
+		return "viewApplyPopup.popup";
+	}
+	@RequestMapping("viewApply.do")
+	public void viewApply(@RequestParam HashMap<String, Object>params){
+		
+	}
+	
+	
+
+}
