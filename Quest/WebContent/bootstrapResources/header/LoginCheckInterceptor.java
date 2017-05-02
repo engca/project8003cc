fa8e6261be09afe299d87d9e08c4680841fb93ba
op7267 <op7267@korea.ac.kr>
2017-05-02 16:51:27 +0900

    intercepter

---
+++ Quest/WebContent/bootstrapResources/header/LoginCheckInterceptor.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class LoginCheckInterceptor extends HandlerInterceptorAdapter{
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+//		return super.preHandle(request, response, handler);
+		if(request.getSession().getAttribute("userId") == null)
+		{
+			response.sendRedirect("listBoard.do");
+			return false;
+		}
+		else
+			return true;
+	}
+}
